bd20c493117d91c0247bb2b9f3cdadc7d49553d8
Diana LN <dianejis@gmail.com>
2021-01-16 12:45:03 +0200

    AQA Homework 09 - Arrays and Collections

--- src/ArrayUtil.java
+++ src/ArrayUtil.java
@@ -1,6 +1,8 @@
 /**
  * ArrayUtil exercises.
  */
+import java.util.*;
+
 public class ArrayUtil {
 
     /**
@@ -11,7 +13,18 @@ public class ArrayUtil {
      * @return String array with common elements
      */
     public static String[] findCommon(String[] array1, String[] array2) {
-        // TODO fill in code here
-        return new String[0];
+        ArrayList list = new ArrayList();
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array2.length; j++) {
+                if (array1[i].equals(array2[j]) && !list.contains(array1[i])) {
+                    list.add(array1[i]);
+                }
+            }
+        }
+        String[] commonItems = new String[list.size()];
+        for(int i = 0; i < commonItems.length; i++){
+            commonItems[i] = (String) list.get(i);
+        }
+        return commonItems;
     }
 }
